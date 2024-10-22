1cfce629b20fe013dd429a844a66939c6b31d0f0
Kapil Gupta <kg666235@gmail.com>
2024-10-22 14:34:45 +0530

    first commit

--- Arrays/TwoSum.java
+++ Arrays/TwoSum.java
@@ -1,19 +1,27 @@
 package Arrays;
 
+import java.util.HashMap;
+
 public class TwoSum {
     public static void main(String[] args) {
         int[] arr = {2, 7, 11, 15};
-        int target = 9;
-        // brute force
-        for(int i = 0;i<arr.length;i++){
-            for(int j = 0;i<arr.length;j++){
-                if(i == j){
-                    continue;
-                }
-                if(arr[i] + arr[j] == target){
-                    System.out.println(arr[i] + arr[j]);
-                }
-            }
+        int target = 17;
+        int[] ans = twoSum(arr, target);
+        for (int i = 0; i < ans.length; i++) {
+            System.out.print(ans[i] + " ");
         }
+    }
+
+    public static int[] twoSum(int[] nums, int target) {
+        // better solution
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            int diff = target - nums[i];
+            if (map.containsKey(diff)) {
+                return new int[]{map.get(diff), i};
+            }
+            map.put(nums[i], i);
+        }
+        return null;
     }
 }
