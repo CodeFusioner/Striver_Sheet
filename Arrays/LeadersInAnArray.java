b898257e6bf875b9d720ffa9a0143f1c789a7a17
Kapil Gupta <kg666235@gmail.com>
2024-11-01 21:15:48 +0530

    first commit

--- Arrays/LeadersInAnArray.java
+++ Arrays/LeadersInAnArray.java
@@ -9,18 +9,29 @@ public class LeadersInAnArray {
     }
     public static List<Integer> leader(int [] arr){
         // brute force
-        List<Integer> list = new ArrayList<>();
-        for(int i = 0;i<arr.length;i++){
-            boolean leader = true;
-            for(int j = i+1;j<arr.length;j++){
-                if(arr[j] > arr[i]){
-                    leader = false;
-                }
+//        List<Integer> list = new ArrayList<>();
+//        for(int i = 0;i<arr.length;i++){
+//            boolean leader = true;
+//            for(int j = i+1;j<arr.length;j++){
+//                if(arr[j] > arr[i]){
+//                    leader = false;
+//                }
+//            }
+//            if(leader){
+//                list.add(arr[i]);
+//            }
+//        }
+//        return list;
+
+        // optimize
+        List<Integer> ll = new ArrayList<>();
+        int max = Integer.MIN_VALUE;
+        for(int i = arr.length-1;i>=0;i--){
+            if(arr[i]>max){
+                ll.add(arr[i]);
             }
-            if(leader){
-                list.add(arr[i]);
-            }
+            max = Math.max(max,arr[i]);
         }
-        return list;
+        return ll;
     }
 }
