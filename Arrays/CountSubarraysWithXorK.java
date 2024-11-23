54ab0573da4afbf4bca45000ab5d58f91e98eced
Kapil Gupta <kg666235@gmail.com>
2024-11-23 15:08:36 +0530

    first commit

---
+++ Arrays/CountSubarraysWithXorK.java
@@ -0,0 +1,25 @@
+package Arrays;
+
+public class CountSubarraysWithXorK {
+    public static void main(String[] args) {
+        int [] arr = {4,2,2,6,4};
+        int K = 6;
+        int count = 0;
+        // brute force
+        for(int i = 0;i<arr.length;i++){
+            for(int j = i;j<arr.length;j++){
+                int xor = 0;
+                for(int k = i;k<j;k++){
+                    xor ^= arr[k];
+                }
+                if(xor == K){
+                    count++;
+                }
+            }
+        }
+        System.out.println(count);
+
+        // better solution
+
+    }
+}
